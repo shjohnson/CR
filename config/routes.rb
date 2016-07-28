cfb5bf444f24c7855978fff30f9f422ef5162947
sh.johnson <sh.johnson@me.com>
2016-07-28 22:37:59 +0100

    WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,16 @@
 Rails.application.routes.draw do
-  get 'user/new'
 
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  get 'user/new'
   root 'home#index'
+
+
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
